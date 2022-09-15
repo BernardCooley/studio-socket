73bfd633ffa737cf9d95a94a414a5bb505f707a2
Bernard.Cooley <Bernard.Cooley@global.com>
2022-09-15 08:03:09 +0100

    move creds

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -1,24 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDqpnfTTpNVL_iIXSWlH09_ltEXmI5sf7Q",
-    authDomain: "studio-socket.firebaseapp.com",
-    projectId: "studio-socket",
-    storageBucket: "studio-socket.appspot.com",
-    messagingSenderId: "411757860372",
-    appId: "1:411757860372:web:a373d125e13f85f8dd7901",
-};
-
-// TODO: Use .env before going live
-// const firebaseConfig = {
-//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-// };
+import { firebaseConfig } from "./credentials";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
